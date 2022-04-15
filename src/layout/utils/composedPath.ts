0de8a8ce001affddbe76094b60a201295872e3a2
Obafemi Lawal <swissbobo@gmail.com>
2022-04-15 06:27:53 +0300

    finally finished the ui for layout fix the mobile bugs on the na logo image with setTimeOut

--- src/layout/utils/composedPath.ts
+++ src/layout/utils/composedPath.ts
@@ -4,23 +4,37 @@ function useCheckToggle(topRef:any, contentRef:any) {
     const [slideIn, setSlideIn] = useState(false)
 
     useEffect(() => {
+        
         const addEvent = (e:any) => {
             e.preventDefault()
+
             if(e.composedPath().includes(topRef.current) || e.composedPath().includes(contentRef.current) ) {
                 
             }
             else if(window.innerWidth < 600){
                 setSlideIn(()=>!slideIn)
+                
             }
+            setTimeout(() => {
+                let navImgSlide = document.querySelector(".image-logo");;
+                navImgSlide?.classList.add("image-logo-animate")
+            },0)
         }
         if(slideIn && window.innerWidth < 600) {
             document.addEventListener('click', addEvent)
+
         }
         return () => document.removeEventListener('click', addEvent)
-    }, [slideIn, topRef, contentRef])
+    }, [slideIn, topRef, contentRef])//useEfect end
     
     const setToggle = function(e:MouseEvent) {
-        setSlideIn(()=>!slideIn)  
+        
+        setSlideIn(()=>!slideIn)
+        setTimeout(() => {
+            let navImgSlide = document.querySelector(".image-logo");;
+            navImgSlide?.classList.add("image-logo-animate")
+        },0)
+        
     }
 
     return [slideIn, setToggle]
